b88861d7d9485fa40dc44a82082d05930e5b4497
Shlomi Fish <shlomif@shlomifish.org>
2019-01-14 19:02:33 +0200

    add docs

--- p5/Dir-Manifest/lib/Dir/Manifest.pm
+++ p5/Dir-Manifest/lib/Dir/Manifest.pm
@@ -57,6 +57,16 @@ sub get_keys
 
 Dir::Manifest - treat a directory and a manifest file as a hash/dictionary of keys to texts or blobs
 
+=head1 DESCRIPTION
+
+Here is the primary use case: you have several long texts (and/or binary blobs) that you
+wish to load from the code (e.g: for the "want"/expected values of tests) and you wish to
+conventiently edit them, track them and maintain them. Using L<Dir::Manifest> you can
+put each in a separate file in a directory, create a manifest file listing all valid
+filenames/key and then say something like
+C<<< my $text = $dir->text("deal24solution.txt", {lf => 1}) >>>. And hopefully it will
+be done securely and reliably.
+
 =head1 METHODS
 
 =head2 $self->manifest_fn()
